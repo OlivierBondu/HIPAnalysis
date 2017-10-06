154ebf39fb38f85b81cff3aced4bc0f3b2ed7c31
Olivier Bondu <olivier.bondu@gmail.com>
2017-10-06 18:05:26 +0200

    looking at calibTrees without nTracks cut

--- scripts/getCalibTreesList.py
+++ scripts/getCalibTreesList.py
@@ -12,6 +12,9 @@ def get_options():
     parser.add_argument('--run', action='store', dest='run', default=[296173], nargs='+', type=int,
                         help='Run number')
     parser.add_argument('-n', '--username', dest='username', help='Remote lxplus username (local username by default)')
+    parser.add_argument('--path', action='store', dest='path', default='/store/group/dpg_tracker_strip/comm_tracker/Strip/Calibration/calibrationtree/GR17/',
+                        type=str, help='path at which find the calibTrees')
+    parser.add_argument('--suffix', action='store', dest='suffix', default='', type=str, help='suffix to be appended to the output file name')
     options = parser.parse_args()
     if options.username is None:
         import pwd, os
@@ -19,15 +22,16 @@ def get_options():
     return options
 
 
-def getCalibTreesList(username, run):
+def getCalibTreesList(username, run, path, suffix):
     lhcFill = utils.get_fill_number(run, username)
     CMSSW_BASE = os.environ['CMSSW_BASE']
-    outfilename = os.path.join(CMSSW_BASE, 'src/', 'CalibTracker/HIPAnalysis', 'test/data', 'list_calibTrees_Fill-%i_Run-%i.json' % (lhcFill, run))
+    if len(suffix) > 0 and (suffix[0] is not '_'):
+        suffix = '_' + suffix
+    outfilename = os.path.join(CMSSW_BASE, 'src/', 'CalibTracker/HIPAnalysis', 'test/data', 'list_calibTrees_Fill-%i_Run-%i%s.json' % (lhcFill, run, suffix))
     if os.path.isfile(outfilename):
         print 'File already exist: %s' % outfilename
         return
 
-    path = '/store/group/dpg_tracker_strip/comm_tracker/Strip/Calibration/calibrationtree/GR17/'
     prefix = '/eos/cms'
     fullpath = prefix + path
     utils.connect_to_lxplus(username)
@@ -48,6 +52,8 @@ def getCalibTreesList(username, run):
 if __name__ == '__main__':
     options = get_options()
     username = options.username
+    path = options.path
+    suffix = options.suffix
     for run in options.run:
-        getCalibTreesList(username, run)
+        getCalibTreesList(username, run, path, suffix)
         print ''
